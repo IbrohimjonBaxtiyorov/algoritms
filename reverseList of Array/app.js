68ced8057b6b79ee102854028819b221cf003cf8
IbrohimjonBaxtiyorov <baxtiyorovibrohim513@gmail.com>
2026-05-17 17:50:24 +0500

    test

--- reverseList of Array/app.js
+++ reverseList of Array/app.js
@@ -10,3 +10,4 @@ function reverseList (head) {
 console.log(reverseList([1,2,3,4,5,15,514,45,45,4,5])); //[5,4,45,45,514,15,5,4,3,2,1]
 console.log(reverseList([12,32,54])) //[54,32,12]
 console.log(reverseList([]))//[]
+conale.log(reverseList([1,2,3,4,5])) //[5,4,3,2,1]
